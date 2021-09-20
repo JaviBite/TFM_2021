9db1d8367953901744aeceb22d116d54c0a2a554
738866 <738866@unizar.es>
2021-09-20 19:03:47 +0200

    json_info.py now export on csv and options as arguments

--- scripts/json_info.py
+++ scripts/json_info.py
@@ -2,80 +2,112 @@
 import json
 import sys
 import codecs, unicodedata
+import csv
 
-def strip_accents(text):
-    try:
-        text = unicode(text, 'utf-8')
-    except NameError: # unicode is a default on python 3 
-        pass
+import argparse
 
-    text = unicodedata.normalize('NFD', text)\
-           .encode('ascii', 'ignore')\
-           .decode("utf-8")
+def parseargs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("json_path", type=str, help="Path to the json of the dataset")
+    parser.add_argument('out_path', nargs='?', default="out.csv", help="Path to the out csv file")
+    parser.add_argument("-m","--mode", default='full', help="Info to excract", choices=['full', 'actions', 'noums'])
+    parser.add_argument("-sm","--sort_mode", default='frec', help="Sorting mode, alphabetical or frequency", choices=['alph', 'frec'])
+    parser.add_argument("-so","--sort_order", default='desc', help="Sorting order (by frequency or alph)", choices=['asc', 'desc'])
 
-    return str(text)
-  
-file1 = sys.argv[1]
+    return parser.parse_args()
 
-# Opening JSON file
-f1 = open(file1, encoding='utf-8')
-  
-# returns JSON object as 
-# a dictionary
-data1 = json.load(f1)
+def main():
 
-actions = {}
-for tag, value in zip(data1['metadata'], data1['metadata'].values()):
-    #print(tag, value)
+    args = parseargs()
 
-    #action = strip_accents(str(value['av']['1'])).lower()
-    action = str(value['av']['1'])
+    json_path = args.json_path
+    out_path = args.out_path
+    mode = args.mode
+    sort_mode = args.sort_mode
 
-    #if action[-1] == 's':
-    #    action = action[0:-1]
+    sort_order = args.sort_order
+    reverse_order = False if sort_order == 'asc' else True
 
-    if action in actions.keys():
-        actions[action] += 1
-    else:
-        actions[action]  = 1
+    file1 = json_path
 
-actions = dict(sorted(actions.items(), reverse=True, key=lambda item: item[1]))
-#actions = dict(sorted(actions.items()))
+    # Opening JSON file
+    f1 = open(file1, encoding='utf-8')
+    
+    # returns JSON object as a dictionary
+    data = json.load(f1)
 
-just_actions = {}
-just_nouns = {}
-for action, times in zip(actions, actions.values()):
-        split_actions = action.split()
-        just_action = split_actions[0] # 0 action 1 ingrediente
-        just_noun = split_actions[1]
+    # Get the full actions
+    actions = {}
+    for tag, value in zip(data['metadata'], data['metadata'].values()):
 
-        if just_action in just_actions.keys():
-            just_actions[just_action] += times
+        action = str(value['av']['1'])
+
+        if action in actions.keys():
+            actions[action] += 1
         else:
-            just_actions[just_action]  = times
+            actions[action]  = 1
 
-        if just_noun in just_nouns.keys():
-            just_nouns[just_noun] += times
-        else:
-            just_nouns[just_noun]  = times
+    if sort_mode == 'frec':
+        actions = dict(sorted(actions.items(), reverse=reverse_order, key=lambda item: item[1]))
+    elif sort_mode == 'alph':
+        actions = dict(sorted(actions.items(), reverse=reverse_order))
 
-just_actions = dict(sorted(just_actions.items(), reverse=True, key=lambda item: item[1]))
-just_nouns = dict(sorted(just_nouns.items(), reverse=True, key=lambda item: item[1]))
-
-#for action, times in zip(just_actions, just_actions.values()):          
-#    print(action, " -> ", times)  
-
-frec1 = 0
-with codecs.open("info.txt", 'w', encoding='utf-8') as out:
+    # Split in actions an noums
+    just_actions = {}
+    just_nouns = {}
     for action, times in zip(actions, actions.values()):
-        if times <= 1:
-            frec1 += 1
+            split_actions = action.split()
+            just_action = split_actions[0] # 0 action 1 ingrediente
+            just_noun = split_actions[1]
 
-          
-        #print(action, " -> ", times)  
-        out.write(action + " -> " + str(times) + "\n")
+            if just_action in just_actions.keys():
+                just_actions[just_action] += times
+            else:
+                just_actions[just_action]  = times
 
-#print("Acciones con frecuencia 1: ", frec1)
+            if just_noun in just_nouns.keys():
+                just_nouns[just_noun] += times
+            else:
+                just_nouns[just_noun]  = times
 
-# Closing file
-f1.close()
+    if sort_mode == 'frec':
+        just_actions = dict(sorted(just_actions.items(), reverse=reverse_order, key=lambda item: item[1]))
+        just_nouns = dict(sorted(just_nouns.items(), reverse=reverse_order, key=lambda item: item[1]))
+    elif sort_mode == 'alph':
+        just_actions = dict(sorted(just_actions.items(), reverse=reverse_order))
+        just_nouns = dict(sorted(just_nouns.items(), reverse=reverse_order))
+
+
+    out_data = None
+
+    if mode == 'full':
+        out_data = actions
+    elif mode == 'actions':
+        out_data = just_actions
+    elif mode == 'noums':
+        out_data = just_nouns
+
+
+    frec1 = 0
+    with codecs.open(out_path, 'w', encoding='utf-8') as out:
+
+        # create the csv writer
+        writer = csv.writer(out)
+
+        for item, times in zip(out_data, out_data.values()):
+            if times <= 1:
+                frec1 += 1
+
+            #out.write(item + ", " + str(times) + "\n")
+            writer.writerow([item ,str(times)])
+
+    #print("Acciones con frecuencia 1: ", frec1)
+
+    # Closing file
+    f1.close()
+    out.close()
+
+    print("Done!")
+
+if __name__ == "__main__":
+    main()
