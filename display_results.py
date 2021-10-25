52ea52aacc3c5b8163fefa9af5afcada5aa49715
738866 <738866@unizar.es>
2021-10-25 19:34:02 +0200

    experiment script

---
+++ display_results.py
@@ -0,0 +1,64 @@
+from matplotlib import pyplot as plt
+from matplotlib import cm
+import json
+import sys
+
+def main():
+
+    #load data
+    f = open(sys.argv[1],)
+    data = json.load(f)
+
+    # Loss
+    fig_loss, ax_loss = plt.subplots()
+    fig_loss.suptitle('Validation Loss', fontsize=20)
+
+    ax_loss.set_xlabel('Epoch')
+    ax_loss.set_ylabel('Loss')
+
+    fig_loss_t, ax_loss_t = plt.subplots()
+    fig_loss_t.suptitle('Train Loss', fontsize=20)
+
+    ax_loss_t.set_xlabel('Epoch')
+    ax_loss_t.set_ylabel('Loss')
+
+    # Accuracy
+    fig_acc, ax_acc = plt.subplots()
+    fig_acc.suptitle('Validation Accuracy', fontsize=20)
+
+    ax_acc.set_xlabel('Epoch')
+    ax_acc.set_ylabel('Accuracy')
+
+    fig_acc_t, ax_acc_t = plt.subplots()
+    fig_acc_t.suptitle('Training Accuracy', fontsize=20)
+
+    ax_acc_t.set_xlabel('Epoch')
+    ax_acc_t.set_ylabel('Accuracy')
+
+    cmap = cm.get_cmap('tab20')
+
+    for indx, experiment in enumerate(data):
+
+        color = cmap(indx/10)
+        color2 = cmap(indx/10 + 1/20)
+
+        lr = experiment['model']['lr']
+        opt = experiment['model']['optimizers']
+
+        label = str(opt) + "lr=" + str(lr)
+
+        history = experiment['history']
+
+        ax_loss_t.plot(history['loss'], label=label, color=color, linewidth=2.0)
+        ax_loss.plot(history['val_loss'], label=label, color=color, linewidth=2.0) 
+
+        ax_acc_t.plot(history['acc'], label=label, color=color, linewidth=2.0)
+        ax_acc.plot(history['val_acc'], label=label, color=color, linewidth=2.0)  
+
+    ax_acc.legend()
+    ax_loss.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
--- net/Bidirectional_LSTM_hog.py
+++ net/Bidirectional_LSTM_hog.py
@@ -6,11 +6,14 @@ Created on Mon Feb 24 09:34:36 2020
 """
 import sys
 from random import random
+import json
 
 from numpy import array
 from numpy import cumsum
 from numpy import array_equal
 import numpy as np
+
+from sklearn.model_selection import train_test_split
 
 from keras.models import Sequential
 from keras.layers import LSTM
@@ -18,102 +21,157 @@ from keras.layers import Dense, Dropout, Flatten
 from keras.layers import TimeDistributed
 from keras.layers import Bidirectional
 from keras.callbacks import EarlyStopping
+from tensorflow.keras.optimizers import SGD, Adam, get
 
 from matplotlib import pyplot
 
+def create_model(num_classes, input_shape, lstm_units, rec_dropout, lstm_act, lstm_rec_act, final_act, hidden_act, dropouts, hidden_dense_untis):
 
-# Load data
-file1 = sys.argv[1]
-files = np.load(file1, allow_pickle=True)
-X, labels = files['a'], files['b']
+    model = Sequential()
+    model.add(Dropout(dropouts[0], input_shape=input_shape)) # (n_timesteps, n_features)
+    model.add(Bidirectional(LSTM(lstm_units, return_sequences=False, activation=lstm_act, recurrent_activation=lstm_rec_act, recurrent_dropout=rec_dropout)))
+    model.add(Dropout(dropouts[1]))
+    #model.add(Flatten())
+    model.add(Dense(hidden_dense_untis, activation=hidden_act))
+    model.add(Dropout(dropouts[2]))
+    model.add(Dense(num_classes, activation = final_act))
 
-num_classes = 2
-y = []
-for yi in labels:
-    to_append = np.zeros(num_classes)
-    to_append[yi] = 1
-    y.append(to_append)
+    return model
 
-y = np.array(y).reshape((len(labels), num_classes))
+def do_experiment(lr, opt, lstm_units, hidden_act, final_act, dropouts, epochs):
 
-print("X Shape: ", X.shape)
-print("Y Shape: ", y.shape)
+    metrics = {}
 
-val_percent = 0.2
-train_percent = 1 - val_percent
+    return metrics
 
-n_train = int(len(labels) * train_percent)
+def main():
+    # Load data
+    file1 = sys.argv[1]
+    files = np.load(file1, allow_pickle=True)
+    X, labels = files['a'], files['b']
 
-trainX, valX = X[:n_train, :, :], X[n_train:, :, :]
-trainy, valy = y[:n_train, :], y[n_train:, :]
+    N_CLASSES = 2
+    y = []
+    for yi in labels:
+        to_append = np.zeros(N_CLASSES)
+        to_append[yi] = 1
+        y.append(to_append)
 
-# define problem
-n_timesteps =  X.shape[1]
-n_sequences =  X.shape[0]
-features = X.shape[2]
+    y = np.array(y).reshape((len(labels), N_CLASSES))
 
-print(features)
-# define LSTM
-model = Sequential()
-model.add(Dropout(0.5, input_shape=(n_timesteps, features)))
-model.add(Bidirectional(LSTM(50, return_sequences=False, activation='relu')))
-model.add(Dropout(0.5))
-#model.add(Flatten())
-model.add(Dense(50, activation="relu"))
-model.add(Dropout(0.3))
-model.add(Dense(num_classes, activation = "relu"))
-model.compile(loss= 'mse' , optimizer= 'adam' , metrics=[ 'acc' ])
-print(model.summary())
+    print("X Shape: ", X.shape)
+    print("Y Shape: ", y.shape)
 
-# train LSTM
-#X, y = get_sequences(n_sequences, 10, size_elem)
+    val_percent = 0.2
+    trainX, valX, trainy, valy = train_test_split(X, y, test_size=val_percent)
 
-print(X.shape)
-print(y.shape)
+    # define problem
+    n_timesteps =  X.shape[1]
+    n_sequences =  X.shape[0]
+    n_features = X.shape[2]
 
-print("Num sequences: ",len(labels))
-for i in range(2):
-    count = np.sum(labels == i)
-    print("Class ",i,":", count)
+    INPUT_SHAPE = (n_timesteps, n_features)
 
-# Early Estopping
-es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=5)
-history = model.fit(trainX, trainy, validation_data=(valX, valy), epochs=50, batch_size=10, callbacks=[es])
+    # do experiments
+    NUM_EXP = 6
 
-model.save('out_model.h5')
+    lr = [0.1, 0.01, 0.001, 0.1, 0.01, 0.001]
+    lstm_units = [50] * NUM_EXP
+    rec_drop = [0.2] * NUM_EXP
+    lstm_act = ['tanh'] * NUM_EXP
+    lstm_rec_act = ['hard_sigmoid'] * NUM_EXP
+    final_act = ['softmax'] * NUM_EXP
+    hidden_act = ['relu'] * NUM_EXP
+    dropouts = [[0.5,0.3,0.2]] * NUM_EXP
+    hidden_dense_untis = [50] * NUM_EXP
 
-# evaluate LSTM
-#X, y = get_sequences(100, n_timesteps, size_elem)
-loss, acc = model.evaluate(valX, valy, verbose=0)
-print( 'Loss: %f, Accuracy: %f '% (loss, acc*100))
+    optimizers = ['adam', 'adam', 'adam', 'sgd', 'sgd', 'sgd']
+    losses = ['categorical_crossentropy'] * NUM_EXP
+    epochs = [30] * NUM_EXP
 
-# make predictions
-#X, y = get_sequences(1, n_timesteps, size_elem)
+    BATCH_SIZE = 10
 
-# Deprecated removed function predict_classes
-# yhat = model.predict_classes(X, verbose=0)
+    models_metrics = []
 
-predict_x=model.predict(X[0:10,:,:]) 
-yhat=np.round(predict_x,decimals=0)
+    # Run the combinations
+    for i in range(NUM_EXP):
 
-#exp, pred = y.reshape(n_timesteps), yhat.reshape(n_timesteps)
+        model = create_model(N_CLASSES, INPUT_SHAPE, lstm_units[i], rec_drop[i], lstm_act[i], 
+                                lstm_rec_act[i], final_act[i], hidden_act[i], dropouts[i], hidden_dense_untis[i])
+        opt = get(optimizers[i])
+        opt.learning_rate = lr[i]
+        model.compile(loss= losses[i] , optimizer= opt , metrics=[ 'acc' ])
+        #print(model.summary())
 
-exp, pred = y[0:10], yhat
-print( 'y=%s, yhat=%s, correct=%s '% (exp, pred, array_equal(exp,pred)))
+        # Early Estopping
+        es = EarlyStopping(monitor='val_loss', mode='min', patience=10)
+        history = model.fit(trainX, trainy, validation_data=(valX, valy), epochs=epochs[i], batch_size=BATCH_SIZE, 
+                                callbacks=[es], shuffle=True, verbose=0)
 
-fig, axs = pyplot.subplots(2, 1, constrained_layout=True)
-axs[0].set_title('Loss')
-axs[0].plot(history.history['loss'], label='train')
-axs[0].plot(history.history['val_loss'], label='test')
-axs[0].legend()
-axs[0].set_xlabel('Epoch')
-axs[0].set_ylabel('Loss')
+        model = {'lr': lr[i],
+                 'lstm_units': lstm_units[i],
+                 'rec_drop': rec_drop[i],
+                 'lstm_act': lstm_act[i],
+                 'lstm_rec_act': lstm_rec_act[i],
+                 'final_act': final_act[i],
+                 'hidden_act': hidden_act[i],
+                 'dropouts': dropouts[i],
+                 'hidden_dense_untis': hidden_dense_untis[i],
+                 'optimizers': optimizers[i],
+                 'losses': losses[i],
+                 'epochs': epochs[i]
+        }
 
-axs[1].set_xlabel('Epoch')
-axs[1].plot(history.history['acc'], label='train')
-axs[1].plot(history.history['val_acc'], label='test')
-axs[1].legend()
-axs[1].set_title('Accuracy')
-axs[1].set_ylabel('Accuracy')
+        metrics = history.history
 
-pyplot.show()
+        to_append = {'model': model, 'history': metrics}
+
+        models_metrics.append(to_append)
+
+    # dumps results
+    out_file = open("out_model_metrics.json", "w")
+    json.dump(models_metrics, out_file, indent=1)
+
+    
+    if False:
+
+        model.save('out_model.h5')
+
+        # evaluate LSTM
+        #X, y = get_sequences(100, n_timesteps, size_elem)
+        loss, acc = model.evaluate(valX, valy, verbose=0)
+        print( 'Loss: %f, Accuracy: %f '% (loss, acc*100))
+
+        # make predictions
+        #X, y = get_sequences(1, n_timesteps, size_elem)
+
+        # Deprecated removed function predict_classes
+        # yhat = model.predict_classes(X, verbose=0)
+
+        predict_x=model.predict(valX) 
+        yhat=np.round(predict_x,decimals=0)
+
+        #exp, pred = y.reshape(n_timesteps), yhat.reshape(n_timesteps)
+
+        exp, pred = valy[0:10], yhat
+        print( 'y=%s, yhat=%s, correct=%s '% (exp, pred, array_equal(exp,pred)))
+
+        fig, axs = pyplot.subplots(2, 1, constrained_layout=True)
+        axs[0].set_title('Loss')
+        axs[0].plot(history.history['loss'], label='train')
+        axs[0].plot(history.history['val_loss'], label='test')
+        axs[0].legend()
+        axs[0].set_xlabel('Epoch')
+        axs[0].set_ylabel('Loss')
+
+        axs[1].set_xlabel('Epoch')
+        axs[1].plot(history.history['acc'], label='train')
+        axs[1].plot(history.history['val_acc'], label='test')
+        axs[1].legend()
+        axs[1].set_title('Accuracy')
+        axs[1].set_ylabel('Accuracy')
+
+        pyplot.show()
+
+if __name__ == "__main__":
+    main()
